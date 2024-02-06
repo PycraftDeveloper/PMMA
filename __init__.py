c26937a41077f1e53a3412239f8f3cf5f757967e
Tom Jebbo <ThomasJebbo@gmail.com>
2024-02-06 09:48:12 +0000

    progress

--- __init__.py
+++ __init__.py
@@ -12,33 +12,21 @@ def up(file_path: str) -> str:
     return file_path[::-1].split(sep, 1)[-1][::-1]
 
 base_path = up(__file__)
+del up
 
 pycache_prefix = base_path+sep+"temporary"
 
-del up
+root = base_path+sep+"src"
+path.append(base_path)
+path.append(root)
+path.append(root+sep+"core")
+path.append(root+sep+"audio")
+path.append(root+sep+"display")
+path.append(root+sep+"events")
+path.append(root+sep+"math")
+path.append(root+sep+"noise")
+path.append(root+sep+"text")
+path.append(root+sep+"time")
+path.append(root+sep+"graphics")
 
-def _init_(sep, path):
-    root = base_path+sep+"src"
-    path.append(base_path)
-    path.append(root)
-    path.append(root+sep+"core")
-    path.append(root+sep+"audio")
-    path.append(root+sep+"display")
-    path.append(root+sep+"events")
-    path.append(root+sep+"math")
-    path.append(root+sep+"noise")
-    path.append(root+sep+"text")
-    path.append(root+sep+"time")
-    path.append(root+sep+"graphics")
-
-if __name__ == "pmma":
-    _init_(sep, path)
-
-    del sep, path, base_path, pycache_prefix
-
-    from main import *
-
-else:
-    _init_(sep, path)
-
-    del sep, path, base_path, pycache_prefix
+del sep, path, pycache_prefix, base_path
