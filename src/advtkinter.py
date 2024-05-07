9cf299a42b7cb3339882a3b9436f0a7ed177ef1d
Thomas Jebson <ThomasJebbo@gmail.com>
2024-05-07 08:48:49 +0100

    extension into tkinter

---
+++ src/advtkinter.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import ttk
+
+from pmma.src.registry import Registry
+from pmma.src.constants import Constants
+
+class TkinterUtils(Registry):
+    def style(self, widget):
+        style = ttk.Style()
+        style.configure(
+            widget,
+            background="white",
+            foreground="black",
+            borderwidth=1,
+            focusthickness=3,
+            focuscolor="none")
+
+        style.map(
+            widget,
+            background=[
+                ("active", "white")])
+
+    def get_display_size(self):
+        try:
+            root = tk.Tk()
+
+            screen_size_x = root.winfo_screenwidth()
+            screen_size_y = root.winfo_screenheight()
+            root.destroy()
+
+            return (
+                screen_size_x,
+                screen_size_y)
+        except:
+            return (0, 0)
+
+    def set_size(self, x, y, root=None):
+        if root is None:
+            root = Registry.root
+
+        screen_size = self.get_display_size()
+        window_size = (x, y)
+        centred_x_position = int((screen_size[0]-window_size[0])/2)
+        centred_y_position = int((screen_size[1]-window_size[1])/2)
+
+        root.geometry(f"{window_size[0]}x{window_size[1]}+{centred_x_position}+{centred_y_position}")
