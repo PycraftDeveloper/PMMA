5c855244de851b697a5fc67bb8a82c80532bb8f8
Thomas Jebson <ThomasJebbo@gmail.com>
2024-05-19 21:43:58 +0100

    big changes

--- src/draw.py
+++ src/draw.py
@@ -4,6 +4,8 @@ import math
 from pmma.src.registry import Registry
 from pmma.src.constants import Constants
 
+tau = math.pi * 2
+
 class Draw(Registry, Constants):
     def __init__(self):
         if Registry.display_mode == Constants.PYGAME:
@@ -12,9 +14,9 @@ class Draw(Registry, Constants):
     def line(self, color, start, end, width):
         if Registry.display_mode == Constants.PYGAME:
             if Registry.anti_aliasing:
-                Registry.graphics_backend.draw.aaline(self.surface, color, start, end, width)
+                return Registry.graphics_backend.draw.aaline(self.surface, color, start, end, width)
             else:
-                Registry.graphics_backend.draw.line(self.surface, color, start, end, width)
+                return Registry.graphics_backend.draw.line(self.surface, color, start, end, width)
         else:
             raise NotImplementedError
 
@@ -23,38 +25,73 @@ class Draw(Registry, Constants):
             if len(points) < 2:
                 return
             if Registry.anti_aliasing:
-                Registry.graphics_backend.draw.aalines(self.surface, color, closed, points, width)
+                return Registry.graphics_backend.draw.aalines(self.surface, color, closed, points, width)
             else:
-                Registry.graphics_backend.draw.lines(self.surface, color, closed, points, width)
+                return Registry.graphics_backend.draw.lines(self.surface, color, closed, points, width)
         else:
             raise NotImplementedError
 
-    def rotated_rect(self, color, center_of_rect, width, height, rotation_angle=0): # https://stackoverflow.com/a/73855696
+    def advanced_polygon(self, color, centre, radius, number_of_sides, rotation_angle=0, width=0, cache=None, wire_frame=False):
+        if cache is not None:
+            if Registry.display_mode == Constants.PYGAME:
+                return Registry.graphics_backend.draw.polygon(self.surface, color, points, width=width), cache
+            else:
+                return None, cache
+
+        if wire_frame:
+            for i in range(0, number_of_sides):
+                if Registry.display_mode == Constants.PYGAME:
+                    Registry.graphics_backend.draw.line(
+                        self.surface,
+                        color,
+                        centre, (
+                            math.cos(i / number_of_sides * tau) * radius + centre[0],
+                            math.sin(i / number_of_sides * tau) * radius + centre[1]))
+
+        points = [
+            (math.cos(i / number_of_sides * tau + rotation_angle) * radius + centre[0],
+                math.sin(i / number_of_sides * tau + rotation_angle) * radius + centre[1]) for i in range(0, number_of_sides)]
+
+        if wire_frame:
+            width = 1
+
+        if Registry.display_mode == Constants.PYGAME:
+            return Registry.graphics_backend.draw.polygon(self.surface, color, points, width=width), points
+        else:
+            return None, cache
+
+    def rotated_rect(self, color, center_of_rect, radius, height, rotation_angle=0, cache=None, width=0): # https://stackoverflow.com/a/73855696
         """Draw a rectangle, centered at x, y.
-        All credit to Tim Swast for this `function that helped make this possible!
+        All credit to Tim Swast for this function!
 
         Arguments:
         x (int/float):
             The x coordinate of the center of the shape.
         y (int/float):
             The y coordinate of the center of the shape.
-        width (int/float):
-            The width of the rectangle.
+        radius (int/float):
+            The radius of the rectangle.
         height (int/float):
             The height of the rectangle.
         color (str):
             Name of the fill color, in HTML format.
         """
+        if cache is not None:
+            if Registry.display_mode == Constants.PYGAME:
+                return Registry.graphics_backend.draw.polygon(self.surface, color, points, width=width), cache
+            else:
+                return None, cache
+
         x, y = center_of_rect
         points = []
 
         # The distance from the center of the rectangle to
         # one of the corners is the same for each corner.
-        radius = math.sqrt((height / 2)**2 + (width / 2)**2)
+        radius = math.sqrt((height / 2)**2 + (radius / 2)**2)
 
         # Get the angle to one of the corners with respect
         # to the x-axis.
-        angle = math.atan2(height / 2, width / 2)
+        angle = math.atan2(height / 2, radius / 2)
 
         # Transform that angle to reach each corner of the rectangle.
         angles = [angle, -angle + math.pi, angle + math.pi, -angle]
@@ -69,13 +106,13 @@ class Draw(Registry, Constants):
             points.append((x + x_offset, y + y_offset))
 
         if Registry.display_mode == Constants.PYGAME:
-            Registry.graphics_backend.draw.polygon(self.surface, color, points)
+            return Registry.graphics_backend.draw.polygon(self.surface, color, points, width=width)
         else:
             raise NotImplementedError
 
     def rect(self, color, rect, width, border_radius=-1, border_top_left_radius=-1, border_top_right_radius=-1, border_bottom_left_radius=-1, border_bottom_right_radius=-1):
         if Registry.display_mode == Constants.PYGAME:
-            Registry.graphics_backend.draw.rect(self.surface, color, rect, width, border_radius, border_top_left_radius, border_top_right_radius, border_bottom_left_radius, border_bottom_right_radius)
+            return Registry.graphics_backend.draw.rect(self.surface, color, rect, width, border_radius, border_top_left_radius, border_top_right_radius, border_bottom_left_radius, border_bottom_right_radius)
         else:
             raise NotImplementedError
 
@@ -83,35 +120,35 @@ class Draw(Registry, Constants):
         if abs(radius) < 1:
             return
         if Registry.display_mode == Constants.PYGAME:
-            Registry.graphics_backend.draw.circle(self.surface, color, center, abs(radius), width)
+            return Registry.graphics_backend.draw.circle(self.surface, color, center, abs(radius), width)
         else:
             raise NotImplementedError
 
     def arc(self, color, rect, start_angle, stop_angle, width=1):
         if Registry.display_mode == Constants.PYGAME:
-            Registry.graphics_backend.draw.arc(self.surface, color, rect, start_angle, stop_angle, width)
+            return Registry.graphics_backend.draw.arc(self.surface, color, rect, start_angle, stop_angle, width)
         else:
             raise NotImplementedError
 
     def polygon(self, color, points, width=0):
         if Registry.display_mode == Constants.PYGAME:
-            Registry.graphics_backend.draw.polygon(self.surface, color, points, width)
+            return Registry.graphics_backend.draw.polygon(self.surface, color, points, width)
         else:
             raise NotImplementedError
 
     def ellipse(self, color, rect, width=0):
         if Registry.display_mode == Constants.PYGAME:
-            Registry.graphics_backend.draw.ellipse(self.surface, color, rect, width)
+            return Registry.graphics_backend.draw.ellipse(self.surface, color, rect, width)
         else:
             raise NotImplementedError
 
     def pixel(self, color, point):
         if Registry.display_mode == Constants.PYGAME:
             try:
-                self.drawing_extension.pixel(self.surface, color, point)
+                return self.drawing_extension.pixel(self.surface, color, point), True
             except:
                 temp_rect = Registry.graphics_backend.rect.Rect(*point, 1, 1)
-                Registry.graphics_backend.draw.rect(self.surface, color, temp_rect, 1)
+                return Registry.graphics_backend.draw.rect(self.surface, color, temp_rect, 1), False
         else:
             raise NotImplementedError
 
@@ -119,11 +156,9 @@ class Draw(Registry, Constants):
         if Registry.display_mode == Constants.PYGAME:
             if len(points) > 2:
                 try:
-                    self.drawing_extension.bezier(self.surface, points, steps, color)
-                    return True
+                    return self.drawing_extension.bezier(self.surface, points, steps, color), True
                 except:
                     pass
-            self.lines(color, points, width=1, closed=False)
-            return False
+            return self.lines(color, points, width=1, closed=False), False
         else:
             raise NotImplementedError
