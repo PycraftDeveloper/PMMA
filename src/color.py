a82839f3080116bf5d8a287666abaaa5d3dff3d6
Thomas Jebson <ThomasJebbo@gmail.com>
2024-05-26 13:09:08 +0100

    WIP more colors

--- src/color.py
+++ src/color.py
@@ -32,10 +32,45 @@ class ColorIntermediary(Registry, Constants):
             color = "".join(color)
             self.color = list(int(str(color[i:i+2]), 16) for i in (0, 2, 4, 6))
 
+    def __convert_rgb_to_hsv(self, red, green, blue):
+        #get rgb percentage: range (0-1, 0-1, 0-1 )
+        red_percentage= red / float(255)
+        green_percentage= green/ float(255)
+        blue_percentage=blue / float(255)
+
+
+        #get hsv percentage: range (0-1, 0-1, 0-1)
+        color_hsv_percentage=colorsys.rgb_to_hsv(red_percentage, green_percentage, blue_percentage)
+
+        #get normal hsv: range (0-360, 0-255, 0-255)
+        color_h=round(360*color_hsv_percentage[0])
+        color_s=round(100*color_hsv_percentage[1])
+        color_v=round(100*color_hsv_percentage[2])
+
+        return color_h, color_s, color_v
+
     def out(self, out_type):
         if sorted(out_type) == sorted(Constants.RGBA):
             return swizzle(Constants.RGBA, self.color, out_type)
         elif sorted(out_type) == sorted(Constants.RGB):
             return swizzle(Constants.RGB, self.color[0:3], out_type)
+        elif sorted(out_type) == sorted(Constants.HSL):
+            color = list(self.__convert_rgb_to_hsv(*self.color[0:3]))
+            return swizzle(Constants.HSL, color, out_type)
+        elif sorted(out_type) == sorted(Constants.HSLA):
+            color = list(self.__convert_rgb_to_hsv(*self.color[0:3])) + [round((100/255)*self.color[3])]
+            return swizzle(Constants.HSLA, color, out_type)
+        elif sorted(out_type) == sorted(Constants.SMALL_HSL):
+            pass
+        elif sorted(out_type) == sorted(Constants.SMALL_HSLA):
+            pass
+        elif sorted(out_type) == sorted(Constants.SMALL_RGB):
+            pass
+        elif sorted(out_type) == sorted(Constants.SMALL_RGBA):
+            pass
+        elif sorted(out_type) == sorted(Constants.HEX):
+            pass
+        elif sorted(out_type) == sorted(Constants.HEXA):
+            pass
 
-print(ColorIntermediary(Constants.RGBA, (250, 190, 170, 5)).out(Constants.BRG))
+print(ColorIntermediary(Constants.RGBA, (250, 190, 170, 255)).out(Constants.LSHA))
