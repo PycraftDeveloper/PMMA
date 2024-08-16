6bdd50be762287dd95e2d4d242b527e0996c180d
Thomas Jebson <ThomasJebbo@gmail.com>
2024-08-16 20:15:35 +0100

    minor tweaks

--- experiments/pmma manipulator.py
+++ experiments/pmma manipulator.py
@@ -7,7 +7,7 @@ base = _up(_up(__file__))
 files = []
 for r, d, f in os.walk(base):
     for file in f:
-        if file.endswith(".py") or file.endswith(".md") or file.endswith(".rst") or file.endswith(".glsl") or file.endswith(".txt") or file.endswith(".pyx") or file.endswith(".c"):
+        if file.endswith(".py") or file.endswith(".md") or file.endswith(".rst") or file.endswith(".glsl") or file.endswith(".txt") or file.endswith(".pyx") or file.endswith(".c") or file.endswith(".toml") or file.endswith(".yml") or file.endswith(".json"):
             files.append(os.path.join(r, file))
 
 line_count = 0
@@ -22,5 +22,6 @@ for file in files:
 
     for line in content:
         line_count += 1
+    print(line_count)
 
 print(line_count)
