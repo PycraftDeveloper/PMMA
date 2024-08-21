be1278311ff045f24750d066d7a6045b3c6e85dc
Thomas Jebson <ThomasJebbo@gmail.com>
2024-08-21 13:20:17 +0100

    minor tweaks

--- experiments/memory management test 2.py
+++ experiments/memory management test 2.py
@@ -10,5 +10,7 @@ init(log_information=True, log_warning=True, log_error=True)
 inst = mm.MemoryManager()
 
 addr = inst.add_object("a", custom_id="test", object_lifetime=15)
-addr = inst.add_object("a", custom_id="shorty", object_lifetime=2)
+addr = inst.add_object("d", custom_id="shorty", object_lifetime=2)
+time.sleep(3)
+addr = inst.add_object("k", custom_id="theinter", object_lifetime=6)
 time.sleep(50)
