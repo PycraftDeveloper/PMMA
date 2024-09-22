47663d3a06efb30b3be765332eb534766f031fc1
Thomas Jebson <ThomasJebbo@gmail.com>
2024-09-22 17:21:44 +0100

    just testing!

--- experiments/audio playback test.py
+++ experiments/audio playback test.py
@@ -9,3 +9,13 @@ audio = pmma.Audio()
 audio.load_from_file(r"H:\Downloads\videoplayback (1).wav")
 
 audio.play(blocking=False)
+
+time.sleep(3)
+
+audio.pause()
+
+time.sleep(1)
+
+audio.resume()
+
+time.sleep(50)
