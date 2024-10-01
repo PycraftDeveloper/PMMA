5694e4ed61fa1ab1ae2d77ecc13a9f47d648aa09
Thomas Jebson <ThomasJebbo@gmail.com>
2024-10-01 17:38:07 +0100

    Update audio playback test.py

--- experiments/audio playback test.py
+++ experiments/audio playback test.py
@@ -8,7 +8,7 @@ pmma.init()
 audio = pmma.Audio()
 audio.load_from_file(r"H:\Downloads\1kHz Sine Wave Test Tone Signal.mp3")
 
-effect = pmma.Reverb(chunk_size=2048)
+effect = pmma.Reverb()
 audio.add_effect(effect)
 
 audio.play(blocking=True)
