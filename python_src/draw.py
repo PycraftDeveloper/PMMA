67c23fcc14009c3bf7988ef23f9cd21d20e17aac
Thomas Jebson <ThomasJebbo@gmail.com>
2024-10-28 21:42:37 +0000

    minor tweak

--- python_src/draw.py
+++ python_src/draw.py
@@ -670,7 +670,7 @@ class Rectangle:
             inner_vertices = inner_vertices[::-1]
             for i in range(len(outer_vertices)):
                 combined_vertices.append(outer_vertices[i])
-                #combined_vertices.append(inner_vertices[i])
+                combined_vertices.append(inner_vertices[i]) # disable this to see inner points viewed as outer points which is WRONG!!!
 
             # Close the shape by adding the first vertices again
             combined_vertices.append(outer_vertices[0])
