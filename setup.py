01fd032afc85ec402fbcca96b8f4850bdf007779
Thomas Jebson <ThomasJebbo@gmail.com>
2024-10-31 16:48:41 +0000

    update and clean up

--- setup.py
+++ setup.py
@@ -2,30 +2,37 @@ from setuptools import setup, find_packages
 from requests import get
 from json import loads
 
+# Get the latest tag from GitHub
 tag_data = get("https://api.github.com/repos/PycraftDeveloper/PMMA/tags")
 latest_tag = loads(tag_data.text)[0]["name"]
 
-with open("README.md", "r", encoding = "utf-8") as fh:
+# Read the long description from README.md
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# Read the requirements from requirements.txt
+with open("requirements.txt", "r", encoding="utf-8") as req_file:
+    requirements = req_file.read().splitlines()
+
 setup(
-    name = "pmma",
-    version = latest_tag,
-    author = "PycraftDev",
-    author_email = "thomasjebbo@gmail.com",
-    description = "Python Multi-Media API (PMMA) is a multi-purpose API designed to make working on multi-media projects easier and faster!",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/PycraftDeveloper/PMMA",
-    project_urls = {
+    name="pmma",
+    version=latest_tag,
+    author="PycraftDev",
+    author_email="thomasjebbo@gmail.com",
+    description="Python Multi-Media API (PMMA) is a multi-purpose API designed to make working on multi-media projects easier and faster!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PycraftDeveloper/PMMA",
+    project_urls={
         "Bug Tracker": "https://github.com/PycraftDeveloper/PMMA/issues",
     },
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir = {"": "src"},
-    packages = find_packages(where="src"),
-    python_requires = ">=3.6"
+    package_dir={"": "."},  # Set root as the package directory
+    packages=find_packages(where="."),  # Find all packages in root and subdirectories
+    python_requires=">=3.6",
+    install_requires=requirements,
 )
