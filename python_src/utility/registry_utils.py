1e1d86a0d8f09fd72654be76dabb320d24ceae51
Thomas Jebson <ThomasJebbo@gmail.com>
2024-11-11 21:11:54 +0000

    perlin noise generation just got really really fast....

--- python_src/utility/registry_utils.py
+++ python_src/utility/registry_utils.py
@@ -2,24 +2,23 @@ from moderngl import Context as _moderngl__Context
 from pprofile import Profile as _pprofile__Profile
 
 class Registry:
-    compile_math_functions = True #
-    running = True #
+    compile_math_functions = True
+    running = True
     temporary_files_path = None
     base_path = None
     custom_compiled_behavior = {}
     cython_acceleration_available = False
     cython_acceleration_enabled = False
-    python_acceleration_enabled = False
     pmma_module_spine = {}
-    do_anti_aliasing = False #
-    anti_aliasing_level = 0 #
-    manually_set_anti_aliasing_level = None #
-    manually_set_do_anti_aliasing = None #
-    development_mode = True #
+    do_anti_aliasing = False
+    anti_aliasing_level = 0
+    manually_set_anti_aliasing_level = None
+    manually_set_do_anti_aliasing = None
+    development_mode = True
     refresh_rate = 60
     display_initialized = False
     context: "_moderngl__Context" = None
-    power_saving_mode = False #
+    power_saving_mode = False
     number_of_render_updates = 0
     total_time_spent_drawing = 0
     pmma_initialized = False
@@ -33,7 +32,7 @@ class Registry:
     window_context = None
     number_of_instantiated_objects = 0
     iteration_id = None
-    language = None #
+    language = None
     opengl_objects = {}
     handled_events = False
     compute_component_called = False
