069a9c76a80b8adac887bf012a3955666ab60039
Thomas Jebson <ThomasJebbo@gmail.com>
2025-02-04 20:57:14 +0000

    pyx alternative improvements

--- python_src/pyx_alternatives/utility/render_pipeline_utils.py
+++ python_src/pyx_alternatives/utility/render_pipeline_utils.py
@@ -1,8 +1,6 @@
 import numpy as np
 import moderngl as _moderngl
 
-from pmma.python_src.opengl import VertexBufferObject as _VertexBufferObject
-from pmma.python_src.opengl import ColorBufferObject as _ColorBufferObject
 from pmma.python_src.opengl import GenericBufferObject as _GenericBufferObject
 from pmma.python_src.opengl import VertexArrayObject as _VertexArrayObject
 from pmma.python_src.opengl import Shader as _Shader
@@ -11,148 +9,87 @@ from pmma.python_src.constants import Constants as _Constants
 
 from pmma.python_src.utility.registry_utils import Registry as _Registry
 
-def repeat_array_cython(base, N):
+def repeat_array_python(base, N):
+    base = base.astype(np.float32)  # Ensure correct dtype
     base_size = base.shape[0]
-    result = np.zeros(N * base_size, dtype=np.float32)
+    result = np.empty(N * base_size, dtype=np.float32)
 
-    # Fill the result array using slicing
     for i in range(N):
-        result[i * base_size : (i + 1) * base_size] = base
+        result[i * base_size: (i + 1) * base_size] = base
 
     return result
 
 class RenderPipeline:
     def __init__(self):
-        self.shapes = []  # Store references to shapes
-
-        self.aspect_ratio = _Registry.pmma_module_spine[_Constants.DISPLAY_OBJECT].get_aspect_ratio() + 1
-
-        # Preallocate memory for vertex, color, and offset data
-        self.vertex_data = np.empty((0,), dtype=np.float32)
-        self.color_data = np.empty((0,), dtype=np.float32)
-        self.offset_data = np.empty((0,), dtype=np.float32)
-
-        # Buffer objects
-        self._vbo = _VertexBufferObject()
-        self._vbo.set_dynamic(True)
-        self._cbo = _ColorBufferObject()
-        self._cbo.set_dynamic(True)
-        self._obo = _GenericBufferObject()
-        self._obo.set_dynamic(True)
+        self.aspect_ratio = _Registry.pmma_module_spine[_Constants.DISPLAY_OBJECT].get_aspect_ratio()
+        self._gbo = _GenericBufferObject()
+        self._gbo.set_dynamic(True)
         self._vao = _VertexArrayObject()
-
-        # Shader
         self._program = _Shader()
         self._program.load_shader_from_folder(_path_builder(_Registry.base_path, "shaders", "render_pipeline"))
         self._program.create()
+        self._total_vertexes = 0
 
     def quit(self):
-        self._shut_down = True
-        self._program.quit(do_garbage_collection=False)
-        self._vao.quit(do_garbage_collection=False)
-        self._vbo.quit(do_garbage_collection=False)
-        self._cbo.quit(do_garbage_collection=False)
-        self._obo.quit(do_garbage_collection=False)
+        self._program.quit()
+        self._vao.quit()
+        self._gbo.quit()
 
-    def update(self, shapes):
-        self.shapes = shapes
+    def  internal_update(self, shape_data, total_data_points):
+        pipeline_data = np.empty(total_data_points, dtype=np.float32)
+        index = 0
 
-        # Compute total size for buffers (including degenerate vertices)
-        total_vertices = 0
-        total_colors = 0
-        total_offsets = 0
-
-        for i in range(len(shapes)):
-            shape = shapes[i]
-
-            vertices = shape._vertex_data
+        for i in range(len(shape_data)):
+            vertices = shape_data[i][0]
             num_points = vertices.shape[0] // 2
-            total_vertices += vertices.shape[0] + 4  # +4 for degenerate vertices (2 per shape)
-            total_colors += num_points * 4 + 8       # +8 for degenerate colors (4 per shape)
-            total_offsets += num_points * 2 + 4     # +4 for degenerate offsets (2 per shape)
+            colors = repeat_array_python(shape_data[i][1], num_points)
+            offsets = repeat_array_python(shape_data[i][2], num_points)
 
-        # Preallocate buffers
-        self.vertex_data = np.empty((total_vertices,), dtype=np.float32)
-        self.color_data = np.empty((total_colors,), dtype=np.float32)
-        self.offset_data = np.empty((total_offsets,), dtype=np.float32)
+            if i > 0:
+                pipeline_data[index:index+8] = pipeline_data[index-8:index]
+                pipeline_data[index+8:index+10] = vertices[:2]
+                pipeline_data[index+10:index+14] = colors[:4]
+                pipeline_data[index+14:index+16] = offsets[:2]
+                index += 16
 
-        # Fill buffers using slicing
-        vertex_index = 0
-        color_index = 0
-        offset_index = 0
+            pipeline_data[index:index + 8 * num_points:8] = vertices[::2]
+            pipeline_data[index + 1:index + 8 * num_points:8] = vertices[1::2]
+            pipeline_data[index + 2:index + 8 * num_points:8] = colors[::4]
+            pipeline_data[index + 3:index + 8 * num_points:8] = colors[1::4]
+            pipeline_data[index + 4:index + 8 * num_points:8] = colors[2::4]
+            pipeline_data[index + 5:index + 8 * num_points:8] = colors[3::4]
+            pipeline_data[index + 6:index + 8 * num_points:8] = offsets[::2]
+            pipeline_data[index + 7:index + 8 * num_points:8] = offsets[1::2]
 
-        for i in range(len(shapes)):
+            index += 8 * num_points
+
+            if i == len(shape_data) - 1:
+                pipeline_data[index:index+8] = pipeline_data[index-8:index]
+                pipeline_data[index+8:index+16] = pipeline_data[index-8:index]
+                index += 16
+
+        self._gbo.set_data(pipeline_data)
+
+    def  update(self, shapes):
+        total_data_points = 0
+        num_shapes = len(shapes)
+        shape_data_list = [None] * num_shapes  # Preallocate list
+
+        for i in range(num_shapes):
             shape = shapes[i]
+            num_points = shape._vertex_data.shape[0] // 2
+            total_data_points += (num_points + 2) * 8
+            shape_data_list[i] = [shape._vertex_data, shape._color_data, shape._offset_data]
 
-            vertices = shape._vertex_data
-            colors = shape._color_data
-            offset = shape._offset_data
-
-            num_points = vertices.shape[0] // 2
-            colors_array = repeat_array_cython(colors, num_points)
-            offset_array = repeat_array_cython(offset, num_points)
-
-            if vertex_index > 0:
-                # Add degenerate vertices to separate shapes
-                # Copy the last vertex of the previous shape
-                self.vertex_data[vertex_index:vertex_index + 2] = self.vertex_data[vertex_index - 2:vertex_index]
-                self.color_data[color_index:color_index + 4] = self.color_data[color_index - 4:color_index]
-                self.offset_data[offset_index:offset_index + 2] = self.offset_data[offset_index - 2:offset_index]
-
-                # Copy the first vertex of the current shape
-                self.vertex_data[vertex_index + 2:vertex_index + 4] = vertices[:2]
-                self.color_data[color_index + 4:color_index + 8] = colors_array[:4]
-                self.offset_data[offset_index + 2:offset_index + 4] = offset_array[:2]
-
-                vertex_index += 4
-                color_index += 8
-                offset_index += 4
-
-            # Add current shape's data
-            self.vertex_data[vertex_index:vertex_index + vertices.shape[0]] = vertices
-            self.color_data[color_index:color_index + colors_array.shape[0]] = colors_array
-            self.offset_data[offset_index:offset_index + offset_array.shape[0]] = offset_array
-
-            vertex_index += vertices.shape[0]
-            color_index += colors_array.shape[0]
-            offset_index += offset_array.shape[0]
-
-            if i == len(shapes) - 1:
-                # Add degenerate vertices for the end of the last shape
-                # Copy the last vertex of the current shape twice to close the shape cleanly
-                self.vertex_data[vertex_index:vertex_index + 2] = vertices[-2:]
-                self.vertex_data[vertex_index + 2:vertex_index + 4] = vertices[-2:]
-                self.color_data[color_index:color_index + 4] = colors_array[-4:]
-                self.color_data[color_index + 4:color_index + 8] = colors_array[-4:]
-                self.offset_data[offset_index:offset_index + 2] = offset_array[-2:]
-                self.offset_data[offset_index + 2:offset_index + 4] = offset_array[-2:]
-
-                vertex_index += 4
-                color_index += 8
-                offset_index += 4
-
-            # Reset shape change flags
-            shape._render_pipeline_color_data_changed = False
-            shape._render_pipeline_vertex_data_changed = False
-            shape._render_pipeline_offset_data_changed = False
-
-        # Upload data to GPU
-        self._vbo.set_data(self.vertex_data)
-        self._cbo.set_data(self.color_data)
-        self._obo.set_data(self.offset_data)
+        self.internal_update(shape_data_list, total_data_points)
 
     def _internal_render(self):
         new_aspect_ratio = _Registry.pmma_module_spine[_Constants.DISPLAY_OBJECT].get_aspect_ratio()
         if new_aspect_ratio != self.aspect_ratio:
-            self._program.set_shader_variable(
-                "aspect_ratio", new_aspect_ratio
-            )
+            self._program.set_shader_variable("aspect_ratio", new_aspect_ratio)
             self.aspect_ratio = new_aspect_ratio
 
-        self._vao.create(
-            self._program,
-            self._vbo, ["2f", "in_position"],
-            color_buffer_object=self._cbo, color_buffer_shader_attributes=["4f", "in_color"],
-            additional_buffers=[self._obo], additional_buffer_attributes=[["2f", "in_offset"]],
-        )
+        if self._vao.get_created() is False:
+            self._vao.create(self._program, self._gbo, ["2f", "in_position", "4f", "in_color", "2f", "in_offset"])
+
         self._vao.render(mode=_moderngl.TRIANGLE_STRIP)
