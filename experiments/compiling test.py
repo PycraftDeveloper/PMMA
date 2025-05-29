d78576190eabf63486ab6b80db6abe335995f9a0
Thomas Jebson <ThomasJebbo@gmail.com>
2025-05-29 18:35:14 +0100

    Removed junk

--- experiments/compiling test.py
+++
@@ -1,3 +0,0 @@
-import pmma
-
-pmma.init()
--- experiments/perlin array test.py
+++
@@ -1,8 +0,0 @@
-import pmma
-import numpy as np
-
-pmma.init()
-
-perlin = pmma.Perlin(seed=0)
-perlin.prefill()
-print(perlin.generate_3D_perlin_noise_from_range([10], [10], [10]))
