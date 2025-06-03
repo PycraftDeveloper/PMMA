56617f4aeceeb096e59fc36402768c0940dc6f90
Thomas Jebson <ThomasJebbo@gmail.com>
2025-06-03 18:57:33 +0100

    WIP

--- setup.py
+++ setup.py
@@ -1,16 +1,30 @@
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 from Cython.Build import cythonize
-import sys, os
-
 import numpy
+import os
+import sys
+import subprocess
 
 cwd = os.path.dirname(__file__)
+build_dir = os.path.join(cwd, "pmma", "build")
+cpp_dir = os.path.join(cwd, "pmma", "core", "cpp_src")
+hpp_dir = os.path.join(cwd, "pmma", "core", "hpp_src")
 
-def add_source(name: str):
-    return [
-        os.path.join(cwd, "pmma", "core", "pyx_src", f"{name}.pyx"),
-        os.path.join(cwd, "pmma", "core", "cpp_src", f"{name}.cpp")
-        ]
+# Shared C++ source files to compile into static library
+cpp_sources = [
+    "Components.cpp",
+    "Registry.cpp",
+    "Display.cpp",
+    "PerlinNoise.cpp"
+]
+cpp_sources = [os.path.join(cpp_dir, f) for f in cpp_sources]
+
+glfw_include = ""
+glfw_lib = ""
+glfw_libraries = []
+compile_args = []
+link_args = []
 
 if sys.platform.startswith("win"):
     compile_args = ["/O2", "/fp:fast", "/GL", "/GF", "/GS-", "/std:c++17"]
@@ -19,99 +33,83 @@ if sys.platform.startswith("win"):
     glfw_include = "H:/Downloads/CPMMA/extern/glfw-3.4.bin.WIN64/include"
     glfw_lib = "H:/Downloads/CPMMA/extern/glfw-3.4.bin.WIN64/lib-vc2022"
     glfw_libraries = [
-        "glfw3",
-        "user32",
-        "gdi32",
-        "shell32",
-        "advapi32",
-        "ole32",
-        "oleaut32",
-        "uuid",
-        "comdlg32",
-        "winmm",
+        "glfw3", "user32", "gdi32", "shell32", "advapi32",
+        "ole32", "oleaut32", "uuid", "comdlg32", "winmm",
     ]
-
 elif sys.platform.startswith("linux"):
-    compile_args = [
-        "-O3", "-ffast-math", "-funroll-loops", "-fstrict-aliasing", "-fomit-frame-pointer", "-std=c++17"
-    ]
-    link_args = []
-
+    compile_args = ["-O3", "-ffast-math", "-funroll-loops", "-fstrict-aliasing", "-fomit-frame-pointer", "-std=c++17"]
     glfw_include = "/usr/include"
     glfw_lib = "/usr/lib/x86_64-linux-gnu"
     glfw_libraries = ["glfw", "GL", "X11", "pthread", "Xrandr", "Xi", "dl", "m"]
-
 elif sys.platform == "darwin":
-    compile_args = [
-        "-O3", "-ffast-math", "-funroll-loops", "-fstrict-aliasing", "-fomit-frame-pointer", "-std=c++17"
-    ]
-    link_args = []
-
-    glfw_include = "/opt/homebrew/include"  # or /usr/local/include depending on installation
+    compile_args = ["-O3", "-ffast-math", "-funroll-loops", "-fstrict-aliasing", "-fomit-frame-pointer", "-std=c++17"]
+    glfw_include = "/opt/homebrew/include"
     glfw_lib = ""
     glfw_libraries = ["glfw", "Cocoa", "OpenGL", "IOKit", "CoreVideo"]
 else:
     raise NotImplementedError("Unsupported platform")
 
-Display_ext = Extension(
-    name="Display",
-    sources=[*add_source("Display"), add_source("Registry")[-1], add_source("Components")[-1]],
-    language="c++",
-    include_dirs=[os.path.join(cwd, "pmma", "core", "hpp_src"), glfw_include, numpy.get_include()],
-    library_dirs=[glfw_lib],
-    libraries=[*glfw_libraries],
-    extra_compile_args=compile_args,
-    extra_link_args=link_args,
-    define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
-)
+# Static library build command
+class BuildStaticLib(build_ext):
+    def run(self):
+        os.makedirs(build_dir, exist_ok=True)
+        objects = []
 
-AdvancedMathematics_ext = Extension(
-    name="AdvancedMathematics",
-    sources=[*add_source("AdvancedMathematics")],
-    language="c++",
-    include_dirs=[os.path.join(cwd, "pmma", "core", "hpp_src"), numpy.get_include()],
-    extra_compile_args=compile_args,
-    extra_link_args=link_args,
-    define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
-)
+        for source in cpp_sources:
+            obj_file = os.path.join(build_dir, os.path.splitext(os.path.basename(source))[0] + ".o")
+            cmd = ["g++" if not sys.platform.startswith("win") else "cl"]
+            cmd += compile_args
+            if not sys.platform.startswith("win"):
+                cmd += ["-c", source, "-o", obj_file, "-I", hpp_dir, "-I", glfw_include]
+            else:
+                obj_file = os.path.join(build_dir, os.path.splitext(os.path.basename(source))[0] + ".obj")
+                cmd += [
+                    "/c", source,
+                    f"/Fo{obj_file}",
+                    f"/I{hpp_dir}",
+                    f"/I{glfw_include}"
+                ]
 
-PerlinNoise_ext = Extension(
-    name="PerlinNoise",
-    sources=[*add_source("PerlinNoise")],
-    language="c++",
-    include_dirs=[os.path.join(cwd, "pmma", "core", "hpp_src"), numpy.get_include()],
-    extra_compile_args=compile_args,
-    extra_link_args=link_args,
-    define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
-)
+            print("Compiling:", " ".join(cmd))
+            subprocess.check_call(cmd)
+            objects.append(obj_file)
 
-FractalBrownianMotion_ext = Extension(
-    name="FractalBrownianMotion",
-    sources=[*add_source("FractalBrownianMotion"), add_source("PerlinNoise")[-1]],  # Reuse PerlinNoise's cpp file
-    language="c++",
-    include_dirs=[os.path.join(cwd, "pmma", "core", "hpp_src"), numpy.get_include()],
-    extra_compile_args=compile_args,
-    extra_link_args=link_args,
-    define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
-)
+        static_lib = os.path.join(build_dir, "libpmma.a")
+        subprocess.check_call(["ar", "rcs", static_lib] + objects)
+        print("Built static library:", static_lib)
 
-NumberConverter_ext = Extension(
-    name="NumberConverter",
-    sources=[add_source("NumberConverter")[0], add_source("Components")[-1]], # This is header only
-    language="c++",
-    library_dirs=[glfw_lib],
-    libraries=[*glfw_libraries],
-    include_dirs=[os.path.join(cwd, "pmma", "core", "hpp_src"), glfw_include, numpy.get_include()],
-    extra_compile_args=compile_args,
-    extra_link_args=link_args,
-    define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
-)
+        # Store for linking
+        self.static_lib = static_lib
+        build_ext.run(self)
+
+def make_ext(name, pyx_name):
+    return Extension(
+        name=name,
+        sources=[os.path.join(cwd, "pmma", "core", "pyx_src", pyx_name + ".pyx")],
+        language="c++",
+        include_dirs=[hpp_dir, glfw_include, numpy.get_include()],
+        library_dirs=[glfw_lib],
+        libraries=glfw_libraries,
+        extra_objects=[os.path.join(build_dir, "libpmma.a")],
+        extra_compile_args=compile_args,
+        extra_link_args=link_args,
+        define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
+    )
+
+ext_modules = [
+    make_ext("Display", "Display"),
+    make_ext("AdvancedMathematics", "AdvancedMathematics"),
+    make_ext("PerlinNoise", "PerlinNoise"),
+    make_ext("FractalBrownianMotion", "FractalBrownianMotion"),
+    make_ext("NumberConverter", "NumberConverter"),
+]
 
 setup(
     name="PMMA",
+    cmdclass={'build_ext': BuildStaticLib},
     ext_modules=cythonize(
-        [Display_ext, AdvancedMathematics_ext, PerlinNoise_ext, FractalBrownianMotion_ext, NumberConverter_ext],
+        ext_modules,
         compiler_directives={"language_level": "3"},
-        annotate=True,  # Optional: creates .html annotation file to inspect performance
+        annotate=True
     ),
 )
