feae6f62b8fcca966c631a805453ed5c73ada8bc
Thomas Jebson <ThomasJebbo@gmail.com>
2025-06-03 20:36:11 +0100

    Revert "WIP"
    This reverts commit 3a378bee6eb788c89bb41ab2e6df1a218be8a0e9.

--- pmma/core/cpp_src/Components.cpp
+++ pmma/core/cpp_src/Components.cpp
@@ -1,13 +1,16 @@
+#include "Components.hpp"
+
+#include "Display.hpp"
+
+namespace CPP_Components {
+    CPP_Display* display = nullptr;
+}
+
 #ifdef _WIN32
 #define EXPORT __declspec(dllexport)
 #else
-#define EXPORT __attribute__((visibility("default")))
+#define EXPORT
 #endif
-
-namespace CPP_Components {
-    // Export the variable (C++ linkage, mangled symbol)
-    EXPORT int value = 10;
-}
 
 extern "C" EXPORT void PMMA_dummy_export() {
     // No-op export
