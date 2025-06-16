93989ca1dcc81f2fad71472f6e4127dcde6e3b69
Thomas Jebson <ThomasJebbo@gmail.com>
2025-06-16 20:08:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -60,7 +60,16 @@ else:
 shared_name = 'PMMA_Core'
 pmma_lib_dir = os.path.join(cwd, "pmma", "lib")
 
-print(pmma_lib_dir)
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+list_files(cwd)
 
 def make_ext(name, extra_cpp=None, add_numpy=False):
     sources = [os.path.join(cwd, "pmma", "core", "pyx_src", f"{name}.pyx")]
