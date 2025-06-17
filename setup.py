8ca03f63b7edfd06306a8830a6e144056b8c9850
Thomas Jebson <ThomasJebbo@gmail.com>
2025-06-17 16:27:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ list_dirs(os.path.join(cwd, "pmma"))
 
 setup(
     name="pmma",
-    version="5.0.4",
+    version="5.0.5",
     author="PycraftDev",
     author_email="thomasjebbo@gmail.com",
     description="Python Multi-Media API (PMMA) is a multi-purpose API designed to make working on multi-media projects easier and faster!",
