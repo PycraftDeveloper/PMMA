8aa0eb20333d1e1ba6a11efa5aca8aadbfaa0412
Thomas Jebson <ThomasJebbo@gmail.com>
2025-06-20 15:11:09 +0100

    From the top

--- setup.py
+++ setup.py
@@ -125,7 +125,7 @@ packages = ['pmma']
 
 setup(
     name="pmma",
-    version="5.0.10",
+    version="5.0.11",
     author="PycraftDev",
     author_email="thomasjebbo@gmail.com",
     description="Python Multi-Media API (PMMA) is a multi-purpose API designed to make working on multi-media projects easier and faster!",
