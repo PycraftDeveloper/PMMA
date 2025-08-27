a08bc0f10659877be9698f9ac676967bf74db7dd
Thomas Jebson <ThomasJebbo@gmail.com>
2025-08-27 21:24:33 +0100

    Improved build process
    TO DO:
    > Tidy up
    > Work on git branch caching (and dynamic build updates)

--- build_tools/main.py
+++ build_tools/main.py
@@ -1,19 +1,55 @@
 # type: ignore
 
 import time
+import shutil
 
 from utils import *
 
 if __name__ == "__main__":
     program_start = time.perf_counter()
 
+    in_github_workflow = os.environ.get("GITHUB_ACTIONS") == "true"
+    build_debug = False
+    rebuild_needed = True # True
+
+    if not in_github_workflow:
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        response = user_input(
+        "Do you want to build a DEBUG version of PMMA? [y/N]: ",
+        "n",
+        5)
+
+        build_debug = response[0].lower() == "y"
+
+    if rebuild_needed:
+        print("🗑️ Removing old build and configuration...")
+        shutil.rmtree(cmake_temp_dir, ignore_errors=True)
+        shutil.rmtree(os.path.join(extern_dir, "lib"), ignore_errors=True)
+        shutil.rmtree(os.path.join(extern_dir, "bin"), ignore_errors=True)
+        shutil.rmtree(os.path.join(extern_dir, "share"), ignore_errors=True)
+        shutil.rmtree(pmma_lib_dir)
+        shutil.rmtree(os.path.join(cwd, "dist"), ignore_errors=True)
+        shutil.rmtree(os.path.join(cwd, "build"), ignore_errors=True)
+        shutil.rmtree(os.path.join(cwd, "pmma.egg-info"), ignore_errors=True)
+        selectively_clean_extern()
+
+    print("🗑️ Removing old logs...")
+    shutil.rmtree(temporary_logging_dir, ignore_errors=True)
+
+    os.makedirs(cmake_temp_dir, exist_ok=True)
+    os.makedirs(extern_dir, exist_ok=True)
+    os.makedirs(temporary_logging_dir, exist_ok=True)
+    os.makedirs(os.path.join(temporary_logging_dir, "dependencies"), exist_ok=True)
+    os.makedirs(pmma_lib_dir, exist_ok=True)
+
     bm = DependencyBuildManager()
 
-    bm.add_component("zlib") # why is this python file not building zlib (it should be built first)
+    bm.add_component("zlib")
     bm.add_component("harfbuzz")
     bm.add_component("libpng", dependencies=["zlib"])
     bm.add_component("glfw")
-    bm.add_component("freetype", dependencies=["zlib", "libpng"])
+    bm.add_component("freetype", dependencies=["zlib", "libpng", "harfbuzz"])
     bm.add_component("bgfx")
 
     bm.build()
@@ -23,8 +59,16 @@ if __name__ == "__main__":
 
     start = time.perf_counter()
 
-    build_pmma()
+    build_pmma(build_debug)
+
+    end = time.perf_counter()
+
+    print(f"🕒 PMMA Build took {end - start:.2f} seconds")
+
+    start = time.perf_counter()
+
+    run_setup(in_github_workflow)
 
     program_end = time.perf_counter()
-    print(f"🕒 PMMA Build took {program_end - start:.2f} seconds")
+    print(f"🕒 Running Setup.py took {program_end - start:.2f} seconds")
     print(f"🕒 Total Build took {program_end - program_start:.2f} seconds")
--- build_tools/utils.py
+++ build_tools/utils.py
@@ -4,20 +4,87 @@ import subprocess
 from collections import deque
 import os
 import threading
+import sys
 
 cwd = os.path.dirname(os.path.dirname(__file__))
 pmma_dir = os.path.join(cwd, "pmma")
+pmma_lib_dir = os.path.join(pmma_dir, "lib")
 temp_dir = os.path.join(cwd, "temporary")
 cmake_temp_dir = os.path.join(temp_dir, "cmake")
 extern_dir = os.path.join(pmma_dir, "extern")
 temporary_logging_dir = os.path.join(temp_dir, "cmake - logs")
 
-os.makedirs(cmake_temp_dir, exist_ok=True)
-os.makedirs(extern_dir, exist_ok=True)
-os.makedirs(temporary_logging_dir, exist_ok=True)
-os.makedirs(os.path.join(temporary_logging_dir, "dependencies"), exist_ok=True)
-
 print_lock = threading.Lock()
+abort = False
+
+def user_input(prompt, default, timeout):
+    result = [default]
+
+    def ask():
+        user_response = input(prompt)
+        result[0] = user_response
+
+    thread = threading.Thread(target=ask)
+    thread.daemon = True
+    thread.start()
+    thread.join(timeout)
+
+    return result[0]
+
+def selectively_clean_extern():
+    if os.path.exists(extern_dir):
+        def should_keep(path):
+            return (f'include{os.sep}glm' in path or
+                    f'include{os.sep}glad' in path or
+                    f'include{os.sep}FlatHashMap' in path or
+                    f'include{os.sep}STB' in path)
+
+        for dirpath, dirnames, filenames in os.walk(extern_dir, topdown=False):
+            full_dirpath = os.path.abspath(dirpath)
+            # Remove files not in keep paths
+            for filename in filenames:
+                full_path = os.path.join(full_dirpath, filename)
+                if not should_keep(full_path):
+                    os.remove(full_path)
+
+            # Remove directories if they're empty and not part of keep paths
+            for dirname in dirnames:
+                full_subdir = os.path.join(full_dirpath, dirname)
+                if not should_keep(full_subdir) and not os.listdir(full_subdir):
+                    os.rmdir(full_subdir)
+
+        # Optionally remove directories that became empty after cleaning
+        if not should_keep(extern_dir) and not os.listdir(extern_dir):
+            os.rmdir(extern_dir)
+
+def selective_removal(directory, keep_items):
+    # Use a list as a queue to process directories iteratively
+    dirs_to_process = [directory]
+
+    while dirs_to_process:
+        current_dir = dirs_to_process.pop(0)
+
+        for item in os.listdir(current_dir):
+            item_path = os.path.join(current_dir, item)
+
+            if os.path.isfile(item_path):
+                # Check if the file should be deleted
+                if not any(item.endswith(keep) for keep in keep_items):
+                    os.remove(item_path)
+
+            elif os.path.isdir(item_path):
+                # Add subdirectory to the queue for later processing
+                if not item_path.endswith(".mypy_cache"):
+                    dirs_to_process.append(item_path)
+
+        # After processing, try to remove empty directories
+        for item in os.listdir(current_dir):
+            item_path = os.path.join(current_dir, item)
+            if os.path.isdir(item_path) and not os.listdir(item_path):
+                try:
+                    os.rmdir(item_path)
+                except Exception as e:
+                    print("Could not delete directory:", item_path, "-", e)
 
 class DependencyBuildManager:
     def __init__(self, base_dir="build_tools/cmake/dependencies"):
@@ -35,6 +102,7 @@ class DependencyBuildManager:
         configure_thread.start()
 
     def configure(self, component):
+        global abort
         folder = os.path.join(cwd, self.base_dir, component)
         with print_lock:
             print(f"⚙️ Configuring {component}...")
@@ -50,9 +118,11 @@ class DependencyBuildManager:
                 stderr=subprocess.STDOUT,
                 text=True)
         except subprocess.CalledProcessError as error:
+            abort = True
             print(f"❌ Error configuring {component}: {error}")
             print("🔍 Output before crash:")
             print(error.output)
+            return
 
         with open(config_log_file, "w") as f:
             f.write("Configuration log:\n")
@@ -101,6 +171,7 @@ class DependencyBuildManager:
         lock = threading.Lock()
 
         def run_build(component):
+            global abort
             folder = os.path.join(cwd, self.base_dir, component)
             if self.configured[component].is_alive():
                 with print_lock:
@@ -120,9 +191,11 @@ class DependencyBuildManager:
                     stderr=subprocess.STDOUT,
                     text=True)
             except subprocess.CalledProcessError as error:
+                abort = True
                 print(f"❌ Error building {component}: {error}")
                 print("🔍 Output before crash:")
                 print(error.output)
+                return
 
             with open(build_log_file, "w") as f:
                 f.write("Build log:\n")
@@ -148,12 +221,22 @@ class DependencyBuildManager:
                 t.start()
                 threads.append(t)
 
+            if abort:
+                print("❌ Aborting build due to errors.")
+                break
+
             # Clean finished threads
             threads = [t for t in threads if t.is_alive()]
 
-        print("🎉 All dependencies built successfully.")
+        if not abort:
+            with print_lock:
+                print("🎉 All dependencies built successfully.")
 
-def build_pmma():
+def build_pmma(build_debug):
+    if abort:
+        print("❌ Aborting PMMA build due to previous errors.")
+        return
+
     folder = os.path.join(cwd, "build_tools/cmake", "pmma")
 
     # Configure PMMA ---------------------------------------------------
@@ -162,17 +245,27 @@ def build_pmma():
 
     config_log_file = os.path.join(temporary_logging_dir, f"pmma-config.log")
     try:
-        configure_result = subprocess.run(
-            [
-                "cmake", "-S", folder, "-B", f"build/pmma",
-                f"-DWORKING_DIR='{cwd}'", "-DCMAKE_BUILD_TYPE=Release"
-            ], check=True, cwd=cmake_temp_dir, stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            text=True)
+        if build_debug:
+            configure_result = subprocess.run(
+                [
+                    "cmake", "-S", folder, "-B", f"build/pmma",
+                    f"-DWORKING_DIR='{cwd}'", "-DCMAKE_BUILD_TYPE=Debug"
+                ], check=True, cwd=cmake_temp_dir, stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                text=True)
+        else:
+            configure_result = subprocess.run(
+                [
+                    "cmake", "-S", folder, "-B", f"build/pmma",
+                    f"-DWORKING_DIR='{cwd}'", "-DCMAKE_BUILD_TYPE=Release"
+                ], check=True, cwd=cmake_temp_dir, stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                text=True)
     except subprocess.CalledProcessError as error:
         print(f"❌ Error configuring pmma: {error}")
         print("🔍 Output before crash:")
         print(error.output)
+        return
 
     with open(config_log_file, "w") as f:
         f.write("Configuration log:\n")
@@ -186,20 +279,74 @@ def build_pmma():
     build_log_file = os.path.join(temporary_logging_dir, f"pmma-build.log")
 
     try:
-        build_result = subprocess.run(
-            [
-                "cmake", "--build", f"build/pmma", "--config", "Release"
-            ], check=True, cwd=cmake_temp_dir, stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            text=True)
+        if build_debug:
+            build_result = subprocess.run(
+                [
+                    "cmake", "--build", f"build/pmma", "--config", "Debug",
+                    "--parallel"
+                ], check=True, cwd=cmake_temp_dir, stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                text=True)
+        else:
+            build_result = subprocess.run(
+                [
+                    "cmake", "--build", f"build/pmma", "--config", "Release",
+                    "--parallel"
+                ], check=True, cwd=cmake_temp_dir, stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                text=True)
     except subprocess.CalledProcessError as error:
         print(f"❌ Error building pmma: {error}")
         print("🔍 Output before crash:")
         print(error.output)
+        return
 
     with open(build_log_file, "w") as f:
         f.write("Build log:\n")
         f.write(build_result.stdout)
 
     with print_lock:
-        print("✅ Finished PMMA")
+        print("🎉 Finished PMMA")
+
+def run_setup(in_github_workflow):
+    if abort:
+        print("❌ Aborting Setup.py due to previous errors.")
+        return
+
+    with print_lock:
+        print("⏳ Started running Setup.py")
+
+    setup_log_file = os.path.join(temporary_logging_dir, f"setup.log")
+
+    try:
+        if in_github_workflow:
+            result = subprocess.run(
+            [
+                sys.executable, "setup.py", "build_ext", "--build-lib",
+                "pmma/build", "--build-temp", "temporary", "sdist",
+                "bdist_wheel"
+            ], check=True, cwd=cmake_temp_dir, stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True)
+        else:
+            result = subprocess.run(
+                [
+                    sys.executable, "setup.py", "build_ext", "--build-lib",
+                    "pmma/build", "--build-temp", "temporary", "sdist",
+                    "bdist_wheel", "--no-parallel", "--annotate_build"
+                ], check=True, cwd=cwd, stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                text=True)
+    except subprocess.CalledProcessError as error:
+        print(f"❌ Error building cython component: {error}")
+        print("🔍 Output before crash:")
+        print(error.output)
+        return
+
+    with open(setup_log_file, "w") as f:
+        f.write("Setup.py log:\n")
+        f.write(result.stdout)
+
+    with print_lock:
+        print("✅ Finished running Setup.py")
+        print("🎉 Finished automated setup process!")
