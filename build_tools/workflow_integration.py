be42ba2b99516c0b4455e08da3a102c52f55409f
Thomas Jebson <ThomasJebbo@gmail.com>
2025-08-30 21:42:18 +0100

    Added workflow integration and updated workflow!

---
+++ build_tools/workflow_integration.py
@@ -0,0 +1,11 @@
+# type: ignore
+
+from deps_utils import *
+from git_utils import *
+
+fetch_cache_branch(True)
+
+for component in previous_hashes:
+    merge_all_subdirs(
+        os.path.join(build_cache_dir, 'cmake', 'dependencies', component, 'build'),
+        extern_dir)
